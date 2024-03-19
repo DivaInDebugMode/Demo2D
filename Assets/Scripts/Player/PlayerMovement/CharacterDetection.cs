e328683a6a249be5440df7c5a9029f83a923cb66
DivaInDebugMode <ograshy@gmail.com>
2024-03-19 00:31:49 -0700

    Demo

---
+++ Assets/Scripts/Player/PlayerMovement/CharacterDetection.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+
+namespace Player.PlayerMovement // Namespace for managing player movement
+{
+    // Class responsible for character detection, inheriting from MonoBehaviour
+    public class CharacterDetection : MonoBehaviour
+    {
+        [Header("Transform")] // Header for organization in Unity Inspector
+        [SerializeField]
+        private Transform groundTransform; // Reference to ground detection transform
+
+        [SerializeField] private Transform headTransformForLedge; // Reference to head transform for ledge detection
+        [SerializeField] private Transform bodyTransformForWall; // Reference to body transform for wall detection
+
+        [field: Header("Ray")] // Header for organization in Unity Inspector
+        public RaycastHit2D HeadRayHit { get; private set; } // RaycastHit2D for head ray hit detection
+
+        private RaycastHit2D checkRayHit; // Temporary RaycastHit2D for ray hit detection
+
+
+        // Properties for move on ledge and input for ledge climb flags
+        public bool HasMoveOnLedge { get; set; }
+        public bool IsInputForLedgeClimb { get; set; }
+        private bool isLedgeParent;
+        private bool isPlatformParent;
+        private bool isDefaultParent;
+        [Header("Class")] // Header for organization in Unity Inspector
+        [SerializeField]
+        private DetectionStats detectionStats; // Reference to detection statistics
+
+        [SerializeField] private CharacterComponents characterComponents; // Reference to character components
+        [SerializeField] private CharacterStats stats;
+
+
+        // Method called on every frame update
+        private void FixedUpdate()
+        {
+            IsWall();
+            IsOnPlatform();
+            IsGrounded(); // Checks if character is on a platform
+            
+        }
+        private void Update()
+        {
+            CheckForLedgeWithRay(); // Checks for ledge presence with ray
+        }
+
+        private void IsGrounded()
+        {
+            // Checks if character's vertical velocity is close to zero and if there's an overlap circle at ground position
+            detectionStats.IsGrounded =  characterComponents.Rb.velocity.y <= 0.01f && Physics2D.OverlapCircle(
+                groundTransform.position, 0.2f, detectionStats.Grounded);
+          
+        }
+        // Method to check if the character is against a wall
+        private void IsWall()
+        {
+            // Checks for wall presence with raycast based on character flip status
+          detectionStats.IsWall =   CheckForWallWithRay() && characterComponents.Sr.flipX == false ||
+                   CheckForWallWithRay() && characterComponents.Sr.flipX;
+        
+        }
+
+        // Method to perform wall detection with raycast
+        private bool CheckForWallWithRay()
+        {
+            // Defines ray direction based on character flip status and casts ray to detect wall
+            Vector2 rayDirection = bodyTransformForWall.right * (characterComponents.Sr.flipX ? -1 : 1);
+            return Physics2D.Raycast(bodyTransformForWall.position, rayDirection,
+                detectionStats.WallCollisionRadius, detectionStats.WallMask);
+            
+        }
+
+        private void CheckForLedgeWithRay()
+        {
+            Vector2 rayDirection = headTransformForLedge.right * (characterComponents.Sr.flipX ? -1 : 1);
+            HeadRayHit = Physics2D.Raycast(headTransformForLedge.position, rayDirection,
+                detectionStats.LedgeLength, detectionStats.Grounded | detectionStats.Platform);
+
+            if (HeadRayHit.collider is null ||
+                (((1 << HeadRayHit.collider.gameObject.layer) & detectionStats.Platform) == 0) ||
+                (detectionStats.IsLedgeDetected || detectionStats.IsGrounded || isLedgeParent)) return;
+            characterComponents.Rb.transform.SetParent(HeadRayHit.transform);
+            isLedgeParent = true;
+        }
+        
+      
+        private void IsOnPlatform()
+        {
+            detectionStats.IsOnPlatform  = characterComponents.Rb.velocity.y <= 0.01f && Physics2D.OverlapCircle(
+                groundTransform.position, 0.2f, detectionStats.Platform);
+            switch (stats.MoveVector.x)
+            {
+                case 0 when detectionStats.IsOnPlatform:
+                {
+                    var platformCollider = Physics2D.OverlapCircle(groundTransform.position, 0.2f, 
+                        detectionStats.Platform);
+                    if (!isPlatformParent)
+                    {
+                        characterComponents.Rb.transform.SetParent(platformCollider.transform);
+                        isPlatformParent = true;
+                    }
+                    break;
+                }
+                default:
+                {
+                    if ((characterComponents.Rb.velocity.x != 0 || characterComponents.Rb.velocity.y !=0 || !detectionStats.IsOnPlatform && ! detectionStats.IsLedgeDetected && !isDefaultParent) && characterComponents.Rb.transform.parent is not null)
+                    {
+                        characterComponents.Rb.transform.SetParent(null);
+                        isDefaultParent = true;
+                        isLedgeParent = false;
+                        isPlatformParent = false;
+                    }
+                    break;
+                }
+            }
+            
+        }
+
+       
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement/DetectionStats.cs
@@ -0,0 +1,77 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+// Namespace for managing player movement
+namespace Player.PlayerMovement
+{
+    // ScriptableObject class for defining detection statistics
+    [CreateAssetMenu(menuName = "Create CharacterDetection", fileName = "CharacterDetection")]
+    public class DetectionStats : ScriptableObject
+    {
+        // Serialized fields for layer masks and detection parameters
+        [Header("Layer Mask")] 
+        [SerializeField] private LayerMask grounded;           // Layer mask for detecting ground
+        [SerializeField] private LayerMask wallMask;           // Layer mask for detecting walls
+        [SerializeField] private LayerMask platform;           // Layer mask for detecting platforms
+
+        [Header("Float")] 
+        [SerializeField] private float wallCollisionRadius;    // Radius for detecting wall collisions
+        [SerializeField] private float ledgeLength;            // Length of ledge
+
+        [Header("Bool")] 
+        [SerializeField] private bool isLedgeDetected;         // Flag indicating if a ledge is detected
+        [SerializeField] private bool isGrounded;
+        [SerializeField] private bool isOnPlatform;
+        [SerializeField] private bool isWall;
+        [SerializeField] private bool isWallJump;
+        [SerializeField] private bool isClimbState;
+
+        // Properties to access serialized fields
+        public LayerMask Grounded => grounded;                 // Property for accessing ground layer mask
+        public LayerMask WallMask => wallMask;                 // Property for accessing wall layer mask
+
+        public bool IsWallJump
+        {
+            get => isWallJump;
+            set => isWallJump = value;
+        }
+        public bool IsGrounded
+        {
+            get => isGrounded;
+            set => isGrounded = value;
+        }
+
+        public bool IsClimbState
+        {
+            get => isClimbState;
+            set => isClimbState = value;
+        }
+
+        public bool IsWall
+        {
+            get => isWall;
+            set => isWall = value;
+        }
+        
+        public bool IsOnPlatform
+        {
+            get => isOnPlatform;
+            set => isOnPlatform = value;
+        }
+        
+        public LayerMask Platform => platform;                 // Property for accessing platform layer mask
+
+        public float WallCollisionRadius
+        {
+            get => wallCollisionRadius;
+            set => wallCollisionRadius = value;
+
+        } // Property for accessing wall collision radius
+        public bool IsLedgeDetected                           // Property for accessing and setting ledge detection flag
+        {
+            get => isLedgeDetected;
+            set => isLedgeDetected = value;
+        }
+        public float LedgeLength => ledgeLength;               // Property for accessing ledge length
+    }
+}
